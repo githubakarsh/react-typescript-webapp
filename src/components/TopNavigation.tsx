e39b9357e3e73e633cf68cac3c4c206b46c5417b
Akarsh Saraff <saraffakarsh@gmail.com>
2021-07-12 18:15:38 +0100

    initialzing project

---
+++ src/components/TopNavigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const TopNavigation = () => {
+    return <header className="header-main">
+        <div>Logo</div>
+        <nav >
+            <ul className="nav-list">
+                <li>Home</li>
+                <li>Option 1</li>
+                <li>Option 2</li>
+            </ul>
+        </nav>
+    </header>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/main.scss';
+
+const component = (<React.StrictMode>
+<App />
+</React.StrictMode>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  component,
   document.getElementById('root')
 );
 
